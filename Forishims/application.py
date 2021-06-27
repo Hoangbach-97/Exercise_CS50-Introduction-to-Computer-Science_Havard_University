0f7d0b284c90048eae1933bb829ba24db819fbf8
Hoangbach-97 <petxuanbach@gmail.com>
2021-06-27 15:50:05 +0700

    Add Forishims Python Basic Flask Framework

---
+++ Forishims/application.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+SPORTS = [
+    "Volleyball",
+    "Soccer",
+    "Badminton",
+    "Shuttlecock Kicking",
+    "Tennis"
+    ]
+
+@app.route("/")
+def index():
+     return render_template("index.html", sports=SPORTS)
+
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    if not request.form.get("name") or not request.form.get("sport") not in SPORTS:
+        return render_template("failure.html")
+    return render_template("success.html")
